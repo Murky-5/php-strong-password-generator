49185fb237552770831014f892c6ef57758f28d0
MirkoDD <mirkodidio111@gmail.com>
2025-04-16 11:30:17 +0200

    non_working_styled_options

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 $cookie = isset($_GET["accesso"]) && !$_GET["accesso"] == "" ? $_GET["accesso"] : false;
+$ripetuti = isset($_GET["ripetute"]) && !$_GET["ripetute"] == "" ? $_GET["ripetute"] : false;
+$simboli = isset($_GET["simboli"]) && !$_GET["simboli"] == "" ? $_GET["simboli"] : false;
+$numeri = isset($_GET["numeri"]) && !$_GET["numeri"] == "" ? $_GET["numeri"] : false;
+$lettere = isset($_GET["lettere"]) && !$_GET["lettere"] == "" ? $_GET["lettere"] : false;
 if ($cookie) {
     $cookie = (int)$cookie;
     if ($cookie > 50) {
@@ -12,15 +16,22 @@ if ($cookie) {
     $cookie = false;
 };
     $_SESSION['cookie'] = $cookie;
+    $_SESSION['ripetute'] = $ripetute;
+    $_SESSION['simboli'] = $simboli;
+    $_SESSION['numeri'] = $numeri;
+    $_SESSION['lettere'] = $lettere;
 
             $stringMax = "Ma_che_bel_gufo_spenzola_da_quei_travi";
             $hashed = password_hash($stringMax, PASSWORD_DEFAULT);
             $hashed = substr($hashed, 7, 59);
+
+    function password($cookie, $ripetuti, $simboli, $numeri, $lettere) {
         if ($cookie) {
             $reqLength = $_SESSION['cookie'];
             while ((int)$cookie < strlen($GLOBALS["hashed"])) {
-                $GLOBALS["hashed"] = substr($hashed, 0, -1);
+                $GLOBALS["hashed"] = substr($GLOBALS["hashed"], 0, -1);
             };
         };
         $_SESSION['cookie'] = $GLOBALS["hashed"];
+    };
         ?>
--- index.php
+++ index.php
@@ -6,23 +6,27 @@
     <title>Pagina d'accesso</title>
     <style>
       body {
-        position: relative;
         display: flex;
         justify-content: center;
         align-items: center;
-        flex-direction: column;
         height: 100vh;
+        flex-wrap: nowrap;
       }
-      form {
-        position: sticky;
-        top: 100px;
-        left: calc(50% - 409.92/2);
+      div {
+        flex-grow: 4;
         align-items: center;
         border: 1px solid black;
         border-radius: 10px;
         margin-bottom: 70px;
         background: grey;
         display: flex;
+      }
+
+      div:nth-child(2){
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
       }
       label {
         font-size: 20px;
@@ -55,8 +59,29 @@
 </head>
 <body>
     <form action="result.php" method="GET">
+      <div>
         <label for="accesso">Lunghezza Desiderata  (Massimo 50)</label>
         <input min=1 max=50 id="accesso" name="accesso" type="number" placeholder="..." />
+      </div>
+      <div>
+        <p>
+        <label for="ripetute">consenti ripetizioni di uno o più caratteri</label>
+    </BR>
+        <input id="ripetute" name="ripetute" type="checkbox"  value="true" placeholder="..." />
+        </p>
+        <p>
+        <input id="lettere" name="lettere" type="checkbox"  value="true" placeholder="..." />
+        <label for="lettere">Lettere</label>
+        </p>
+        <p>
+        <input id="numeri" name="numeri" type="checkbox" value="true"  placeholder="..." />
+        <label for="numeri">Numeri</label>
+        </p>
+        <p>
+        <input id="simboli" name="simboli" type="checkbox" value="true"  placeholder="..." />
+        <label for="simboli">Simboli</label>
+        </p>
+      </div>
         <button type="submit">Crea</button>
         </br>
         </form>
